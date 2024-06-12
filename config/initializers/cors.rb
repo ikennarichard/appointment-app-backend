7752e855e883db6750e0ae3fb34058dd10f28a44
Ikenna Richard <oguejioforrichard@gmail.com>
2024-06-12 13:44:05 +0100

    Add react dev url

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://morb-appointment-app.netlify.app'
+    origins ['https://morb-appointment-app.netlify.app', 'localhost:5173']
 
     resource '*',
              headers: :any,
